c5eae7a2d1a058acccddd182becbe4eff1eb00c7
Dayro Longas <dgiovannyl@gmail.com>
2018-09-10 10:02:03 -0500

    Add base structure.

---
+++ crowdin/src/app/app.module.ts
@@ -0,0 +1,16 @@
+
+import { LOCALE_ID, NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [ { provide: LOCALE_ID, useValue: 'fr' } ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
